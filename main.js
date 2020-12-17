079069649e57ae052fe2b5bd604c14efaec2a565
Allam9573 <aallamdaniel@gmail.com>
2020-12-17 14:45:27 -0600

    introduje dos cards al html y elimine for en main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const todos = [];
 
-
 window.onload = () => {
     const form = document.getElementById('todo-form');
     form.onsubmit = (e) => {
@@ -10,11 +9,9 @@ window.onload = () => {
         task.value = '';
         todos.push(taskText);
         const todoList = document.getElementById('todo-list');
-        todoList.innerHTML = '';
-        for (let i = 0; i < todos.length; i++) {
-            
-            todoList.innerHTML += '<li>' + todos[i] + '</li>';
-        }
+        const todosTemplate = todos.map(t => "<li>" + t + "</li>");
+        todoList.innerHTML = todosTemplate.join('');
+
     }
     const btnAdd = document.getElementById('btnAdd');
 
